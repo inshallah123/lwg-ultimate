a2eba36570e84730583b8a33eb22e988175406b0
yyh <yangyihua06@gmail.com>
2025-08-26 14:03:19 +0800

    修复ide报错

--- src/types/electron.d.ts
+++ src/types/electron.d.ts
@@ -1,5 +1,35 @@
+import { Event } from './event';
+
+interface UpdateProgressData {
+  bytesPerSecond: number;
+  percent: number;
+  transferred: number;
+  total: number;
+}
+
 declare global {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface Window {
+    electronAPI: {
+      database: {
+        getAllEvents: () => Promise<Event[]>;
+        addEvent: (event: Event) => Promise<void>;
+        updateEvent: (event: Event) => Promise<void>;
+        deleteEvent: (id: string) => Promise<void>;
+        syncEvents: (events: Event[]) => Promise<void>;
+      };
+      onUpdateProgress: (callback: (event: any, data: UpdateProgressData) => void) => void;
+      onUpdateComplete: (callback: () => void) => void;
+      removeUpdateListeners: () => void;
+    };
+    electron: {
+      ipcRenderer: {
+        invoke: (channel: string, ...args: any[]) => Promise<any>;
+        on: (channel: string, listener: (event: any, ...args: any[]) => void) => void;
+        removeAllListeners: (channel: string) => void;
+        send: (channel: string, ...args: any[]) => void;
+      };
+    };
+  }
 }
 
 export {};
